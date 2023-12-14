c8c243eb8fb35327257362ae5e81390408e89053
zakyikow <zakyikow@gmail.com>
2023-12-14 16:43:53 +0100

    4 commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const jwt = require('jsonwebtoken');
-require('dotenv').config({ path: '../S_KEYS/.env' });
+require('dotenv').config({ path: './S_KEYS/.env' });
 
 
 // handle errors
